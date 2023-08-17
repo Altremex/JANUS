6c05920b1762fab6e7a99fb548b23ad7a4978e8a
Altremex <134339210+Altremex@users.noreply.github.com>
2023-08-17 13:06:37 -0600

    Add files via upload

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,81 +1,80 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const inputElements = document.querySelectorAll("input");
+  inputElements.forEach(input => {
+    input.addEventListener("input", handleInputs);
+  });
+
+  const calcularRendimientoBtn = document.getElementById("calcularRendimientoBtn");
+  calcularRendimientoBtn.addEventListener("click", calculateCompoundInterest);
+
+  calculateCompoundInterest();
+  calcularNivelRiesgo();
+});
+
+// Función para manejar los cambios en los campos de entrada
+function handleInputs() {
+  calculateCompoundInterest();
+  calcularNivelRiesgo();
+}
+
 // Función para calcular el rendimiento anual basado en el saldo inicial
 function calcularRendimientoAnual(saldoInicial) {
-    const ranges = [
-      { min: 100000000, percentage: 4.5 },
-      { min: 500000, percentage: 2.8 },
-      { min: 250000, percentage: 2.4 },
-      { min: 100000, percentage: 2.1 },
-      { min: 70000, percentage: 1.5 },
-      { min: 40000, percentage: 1 }
-    ];
-  
-    let rendimientoAnual = 0;
-    for (const range of ranges) {
-      if (saldoInicial >= range.min) {
-        rendimientoAnual = range.percentage;
-        break;
-      }
+  const ranges = [
+    { min: 100000000, percentage: 4.5 },
+    { min: 500000, percentage: 2.8 },
+    { min: 250000, percentage: 2.4 },
+    { min: 100000, percentage: 2.1 },
+    { min: 70000, percentage: 1.5 },
+    { min: 40000, percentage: 1 }
+  ];
+
+  let rendimientoAnual = 0;
+  for (const range of ranges) {
+    if (saldoInicial >= range.min) {
+      rendimientoAnual = range.percentage;
+      break;
     }
-  
-    return rendimientoAnual;
   }
-  
+
+  return rendimientoAnual;
+}
+
 // Función para calcular el interés compuesto
 function calculateCompoundInterest() {
-    const initialBalance = parseFloat(document.getElementById("initialBalance").value);
-    const years = parseFloat(document.getElementById("plazoMeses").value) / 12;
+  const initialBalance = parseFloat(document.getElementById("initialBalance").value);
+  const years = parseFloat(document.getElementById("plazoMeses").value) / 12;
 
-    const rendimientoAnualPorcentaje = calcularRendimientoAnual(initialBalance);
+  const rendimientoAnualPorcentaje = calcularRendimientoAnual(initialBalance);
 
-    let balance = initialBalance;
+  let balance = initialBalance;
 
-    for (let year = 1; year <= years; year++) {
-        balance *= (1 + rendimientoAnualPorcentaje / 100);
-    }
-
-    const totalBalance = balance.toFixed(2);
-
-    document.getElementById("result").textContent = `Saldo total: $${totalBalance}\nTasa de interés anual: ${rendimientoAnualPorcentaje.toFixed(2)}%`;
-    document.getElementById("rendimientoAnual").textContent = `${rendimientoAnualPorcentaje.toFixed(2)}%`;
-}
-  
-  
-  
-  // Función para calcular el nivel de riesgo
-  function calcularNivelRiesgo() {
-    const percepcionInput = document.getElementById("percepcionRiesgo");
-    const nivelRiesgoSpan = document.getElementById("nivelRiesgo");
-    const percepcion = parseFloat(percepcionInput.value);
-  
-    let nivel = "";
-  
-    if (percepcion >= 5 && percepcion <= 10) {
-      nivel = "Conservador";
-    } else if (percepcion > 10 && percepcion <= 20) {
-      nivel = "Moderado";
-    } else if (percepcion > 20 && percepcion <= 40) {
-      nivel = "Agresivo";
-    } else {
-      nivel = "N/A";
-    }
-  
-    nivelRiesgoSpan.textContent = nivel;
+  for (let year = 1; year <= years; year++) {
+    balance *= 1 + rendimientoAnualPorcentaje / 100;
   }
-  
-  // Inicialización de eventos
-  document.addEventListener("DOMContentLoaded", function() {
-    const inputElements = document.querySelectorAll("input");
-    inputElements.forEach(input => {
-      input.addEventListener("input", calculateCompoundInterest);
-    });
-  
-    const percepcionInput = document.getElementById("percepcionRiesgo");
-    percepcionInput.addEventListener("input", calcularNivelRiesgo);
-  
-    const calcularRendimientoBtn = document.getElementById("calcularRendimientoBtn");
-    calcularRendimientoBtn.addEventListener("click", calculateCompoundInterest);
-  
-    calculateCompoundInterest();
-    calcularNivelRiesgo();
-  });
-  
+
+  const totalBalance = balance.toFixed(3);
+
+  document.getElementById("result").textContent = `Saldo total: $${totalBalance}\nTasa de interés anual: ${rendimientoAnualPorcentaje.toFixed(2)}%`;
+  document.getElementById("rendimientoAnual").textContent = `${rendimientoAnualPorcentaje.toFixed(2)}%`;
+}
+
+// Función para calcular el nivel de riesgo
+function calcularNivelRiesgo() {
+  const percepcionInput = document.getElementById("percepcionRiesgo");
+  const nivelRiesgoSpan = document.getElementById("nivelRiesgo");
+  const percepcion = parseFloat(percepcionInput.value);
+
+  let nivel = "";
+
+  if (percepcion >= 5 && percepcion <= 10) {
+    nivel = "Conservador";
+  } else if (percepcion > 10 && percepcion <= 20) {
+    nivel = "Moderado";
+  } else if (percepcion > 20 && percepcion <= 40) {
+    nivel = "Agresivo";
+  } else {
+    nivel = "N/A";
+  }
+
+  nivelRiesgoSpan.textContent = nivel;
+}
